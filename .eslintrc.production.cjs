32cbb9d7eec56bf5d8ed7ae8ffc1d663e90c40e7
ryanmaclean <noreply@ryanmaclean.com>
2025-09-03 21:27:24 -0700

    fix: remove problematic ESLint disable comments - Remove @typescript-eslint/no-explicit-any disable comments - Remove @typescript-eslint/no-require-imports disable comments - Remove @typescript-eslint/no-var-requires disable comments - Update production ESLint config to only include available rules - Lint now passes with only 1 warning (unused disable directive)

--- .eslintrc.production.cjs
+++ .eslintrc.production.cjs
@@ -8,17 +8,14 @@ module.exports = [
   {
     rules: {
       // Disable all problematic rules for production deployment
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-require-imports': 'off',
-      'react-hooks/exhaustive-deps': 'off',
       'no-console': 'off',
       'no-debugger': 'off',
       'no-unreachable': 'off',
       'no-duplicate-imports': 'off',
       '@next/next/no-img-element': 'off',
       'import/no-anonymous-default-export': 'off',
-      'jsx-a11y/alt-text': 'off'
+      'jsx-a11y/alt-text': 'off',
+      'react-hooks/exhaustive-deps': 'off'
     }
   }
 ];
