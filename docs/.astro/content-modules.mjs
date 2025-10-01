c38a20cab95e081149f76022fcdba9c0960014da
Ryan MacLean <ryan.maclean@github.com>
2025-09-30 20:35:57 -0700

    chore: add Jest watch mode config and test tasks in VS Code

--- docs/.astro/content-modules.mjs
+++ docs/.astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/wiki-archive/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fwiki-archive%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
