fea8b3509e78d7621c60bb9e8e44f79dde4b4da7
studio <studio@minim4-24.local>
2026-02-28 01:46:59 -0800

    auto-claude: subtask-4-1 - Remove legacy ESLint config files

--- .eslintrc.temp.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['next/core-web-vitals'],
-  rules: {
-    'no-console': 'off',
-    'no-undef': 'off',
-    'no-control-regex': 'off',
-    '@typescript-eslint/no-unused-vars': 'off'
-  }
-};
